dd9ea1d51654ffe914bcb45455e3712524e595a7
Christopher Kaag <christopher.kaag@gmail.com>
2018-03-15 22:31:15 +0100

    url persistent key selection and url params now working on client side

--- js/src/main/kotlin/krese/ClientState.kt
+++ js/src/main/kotlin/krese/ClientState.kt
@@ -9,45 +9,68 @@ import krese.data.UniqueReservableKey
 import org.w3c.xhr.XMLHttpRequest
 import kotlinx.html.*
 import kotlinx.html.js.onClickFunction
+
+import kotlinx.html.*
+import kotlinx.html.consumers.*
+import kotlinx.html.dom.*
+import kotlinx.html.js.*
 import org.w3c.dom.*
+import kotlin.browser.*
+import kotlin.dom.*
+import kotlinx.html.*
+import org.w3c.dom.*
+import org.w3c.dom.events.Event
 import kotlin.browser.document
 import kotlin.js.Date
 
+
+external fun encodeURIComponent(str: String): String
+
+external fun decodeURIComponent(str: String): String
+
+
 class ClientState {
-    private val creationDate = Date().getMilliseconds()
+    private val creationDate = Date.now()
+
+    init {
+        println("creationDate =" + Date(creationDate))
+    }
+
     var from = Date(creationDate - 1000L * 60 * 60 * 24 * 180)
     var to = Date(creationDate + 1000L * 60 * 60 * 24 * 180)
 
     var selectedKey: UniqueReservableKey? = null
     var jwt: String? = null
     var allKeys: List<UniqueReservableKey> = listOf()
-    val entries : MutableMap<UniqueReservableKey, GetResponse> = mutableMapOf()
-
+    val entries: MutableMap<UniqueReservableKey, GetResponse> = mutableMapOf()
 
 
     val navbar = document.getElementById("key-tab-nav-bar")!!
     val tabcontainer = document.getElementById("key-tab-container")!!
 
+    val SELECTED_KEY_URL_KEY = "selected_key"
+
     init {
         loadAllKeys()
-    }
-
-    fun parseURLParameters() {
+        //setURLParameters(mapOf("myparam1" to "value1", "myparam2" to "value2"))
+        //println("Params:")
+        //val params = getURLParameters()
+        //params.forEach { println("${it.key} = ${it.value}") }
 
     }
 
     fun switchTo(uniqueReservableKey: UniqueReservableKey) {
-        val x : HTMLCollection = document.getElementsByClassName("city")
+        val x: HTMLCollection = document.getElementsByClassName("city")
 
         selectedKey = uniqueReservableKey
 
         loadEntriesToKey(selectedKey!!, from, to)
 
         0.until(x.length).map {
-            val k : HTMLDivElement = x.get(it)!! as HTMLDivElement
+            val k: HTMLDivElement = x.get(it)!! as HTMLDivElement
             k.style.display = "none"
         }
-        (document.getElementById("div_"+uniqueReservableKey.id)!! as HTMLDivElement).style.display = "block";
+        (document.getElementById("div_" + uniqueReservableKey.id)!! as HTMLDivElement).style.display = "block";
     }
 
     private fun loadAllKeys() {
@@ -57,15 +80,20 @@ class ClientState {
             if (xhttp.readyState == 4.toShort() && xhttp.status == 200.toShort()) {
                 allKeys = JSON.parse<GetTotalResponse>(xhttp.responseText).keys
                 setAllKeysToGUI()
+
+                getURLParameters().get(SELECTED_KEY_URL_KEY)?.let { switchTo(UniqueReservableKey(it)) }
             }
         }
         xhttp.send()
     }
 
-    private fun transformKeyIntoButton(key: UniqueReservableKey) : HTMLElement {
+    private fun transformKeyIntoButton(key: UniqueReservableKey): HTMLElement {
         return document.create.button {
             classes = setOf("w3-bar-item", "w3-button")
-            onClickFunction = { switchTo(key) }
+            onClickFunction = {
+                addURLParameter(SELECTED_KEY_URL_KEY, key.id)
+                switchTo(key)
+            }
             +"Button for ${key.id}"
         }
     }
@@ -83,7 +111,7 @@ class ClientState {
          */
 
         return document.create.div {
-            id = "div_"+key.id
+            id = "div_" + key.id
             style = "display:none"
             classes = setOf("w3-container", "city")
             div {
@@ -120,6 +148,7 @@ class ClientState {
         allKeys.sortedBy { it.id }.forEach {
             navbar.appendChild(transformKeyIntoButton(it))
             tabcontainer.appendChild(transformKeyIntoDiv(it))
+            addDatePickers(document.getElementById("dap_" + it.id)!!)
         }
     }
 
@@ -139,9 +168,114 @@ class ClientState {
         println("setting all entries to key ${uniqueReservableKey.id}")
         document.getElementById("pro_" + uniqueReservableKey.id)!!.innerHTML = entries.get(uniqueReservableKey)!!.reservable.prologueMarkdown
         document.getElementById("epi_" + uniqueReservableKey.id)!!.innerHTML = entries.get(uniqueReservableKey)!!.reservable.epilogueMarkdown
-        document.getElementById("lis_" + uniqueReservableKey.id)!!.innerHTML = JSON.stringify( entries.get(uniqueReservableKey)!!.existingReservations)
+        document.getElementById("lis_" + uniqueReservableKey.id)!!.innerHTML = (entries.get(uniqueReservableKey)!!.existingReservations).toString()
+        addFormular(document.getElementById("for_" + uniqueReservableKey.id)!!, uniqueReservableKey)
 
-
-        //TODO: add data to skeleton
     }
+
+    fun addFormular(formDiv: Element, uniqueReservableKey: UniqueReservableKey) {
+        formDiv.innerHTML = ""
+        formDiv.appendChild(
+
+                document.create.form(action = null, encType = null,
+                        method = null) {
+                    id = "submitform_${uniqueReservableKey.id}"
+                    onSubmitFunction = {
+                        if (formSubmit(uniqueReservableKey, it)) {
+                            (document.getElementById("submitform_${uniqueReservableKey.id}")!! as HTMLFormElement).reset()
+                        }
+                        it.preventDefault()
+                        it.stopPropagation()
+                    }
+                    input(type = InputType.text, name = "field1")
+                    br()
+                    input(type = InputType.text, name = "field2")
+                    br()
+                    input(type = InputType.checkBox, name = "field3")
+                    br()
+
+                    submitInput { }
+                }
+        )
+    }
+
+    fun formSubmit(key: UniqueReservableKey, formEvent: Event): Boolean {
+        println("formSubmit is checked: ")
+        console.log(formEvent)
+        return true //TODO: implement checking of form data, and AJAX post if successful
+    }
+
+    fun addDatePickers(formDiv: Element) {
+        formDiv.appendChild(
+                document.create.form(action = null, encType = null,
+                        method = null) {
+                    onSubmitFunction = {
+                        selectedKey?.let { it1 -> loadEntriesToKey(it1, from, to) }
+                        it.preventDefault()
+                        it.stopPropagation()
+                    }
+                    label {
+                        +"from:"
+                    }
+                    input(type = InputType.date, name = "from-picker") {
+                        value = from.toDateShort()
+                        onChangeFunction = {
+                            from = Date(this.value)
+                        }
+                    }
+                    br()
+                    label {
+                        +"to:"
+                    }
+                    input(type = InputType.date, name = "to-picker") {
+                        value = to.toDateShort()
+                        println("to.toUTCString() = " + to.toDateShort())
+                        onChangeFunction = {
+                            to = Date(this.value)
+                        }
+                    }
+                    button {
+                        type = ButtonType.submit
+                        +"Change Filter"
+                    }
+                }
+        )
+    }
+
+
+    fun Date.toDateShort(): String = this.getFullYear().toString() + "-" + ("0" + (this.getMonth() + 1)).takeLast(2) + "-" + ("0" + this.getDate()).takeLast(2)
+
+
+    fun setURLParameters(params: Map<String, String>) {
+        val frontstr = window.location.href.takeWhile { it != '?' }
+        val paramstr: String = params.toList().map {
+            encodeURIComponent(it.first) + "=" + encodeURIComponent(it.second)
+        }.joinToString("&")
+
+        window.history.pushState(null, "Krese Redirect", frontstr + if (paramstr.isBlank()) "" else "?" + paramstr)
+        //causes reloads: window.location.href =
+    }
+
+
+    fun addURLParameter(key: String, value: String) {
+        setURLParameters(getURLParameters() + (key to value))
+    }
+
+    fun getURLParameters(): Map<String, String> {
+        var paramstr = window.location.href.dropWhile { it != '?' }
+        if (paramstr.startsWith("?")) {
+            paramstr = paramstr.substring(1)
+        }
+
+        return paramstr.split("&").filter { it.isNotBlank() }.map { kvpair ->
+            val arr = kvpair.split("=")
+            require(arr.size == 2)
+            val key = decodeURIComponent(arr[0])
+            val value = decodeURIComponent(arr[1])
+
+            key to value
+        }.toMap()
+    }
+
+
 }
